e9ed5a078ad4571bf111767af8773bcb49413a63
SrinivasPrasad <43960959+SrinuSandy@users.noreply.github.com>
2025-06-06 21:31:44 +0530

    Fix the ball tailing issues

--- Source/Core/GameWindowManager.cpp
+++ Source/Core/GameWindowManager.cpp
@@ -29,6 +29,7 @@ namespace Core
 
 	void GameWindowManager::clearGameWindow() 
 	{
+		game_window->clear();
 		//game_window->clear(Color(200, 50, 50, 255)); // if want red window uncomment it
 	}
 
