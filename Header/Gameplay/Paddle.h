9034ab623f2b47d31fb82cc000d5c085f0edf324
SrinivasPrasad <43960959+SrinuSandy@users.noreply.github.com>
2025-06-07 07:41:27 +0530

    Implement framrate independent movment

--- Header/Gameplay/Paddle.h
+++ Header/Gameplay/Paddle.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "../Utility/TimeService.h"
 using namespace sf;
+using namespace Utility;
 
 
 namespace Gameplay 
@@ -12,11 +14,13 @@ namespace Gameplay
 		float paddle_width = 20.0f;
 		float paddle_height = 140.0f;
 
-		const float paddleSpeed = 0.8f;
+		const float paddleSpeed = 5.0f;
 		float topBoundary = 20.0f;
 		float bottomBoundary = 700.0f;
 
-		void movePaddle(bool move_up_key_pressed, bool move_down_key_pressed);
+		int speedMultiplier = 150;
+
+		void movePaddle(bool move_up_key_pressed, bool move_down_key_pressed, TimeService* time_service);
 		void createPaddle(float position_x, float position_y);
 
 	public:
@@ -25,7 +29,7 @@ namespace Gameplay
 		RectangleShape getPaddleSprite();
 		void reset(float position_x, float position_y);
 
-		void update(bool move_up_key_pressed, bool move_down_key_pressed);
+		void update(bool move_up_key_pressed, bool move_down_key_pressed, TimeService* time_service);
 		void render(RenderWindow* game_window);
 	};
 }
--- Source/Gameplay/Paddle.cpp
+++ Source/Gameplay/Paddle.cpp
@@ -1,7 +1,5 @@
 #include "../../Header/Gameplay/Paddle.h"
 
-
-
 namespace Gameplay
 {
 	Paddle::Paddle(float position_x, float position_y)
@@ -15,21 +13,21 @@ namespace Gameplay
 		game_window->draw(paddle_sprite);
 	}
 
-	void Paddle::movePaddle(bool move_up_key_pressed, bool move_down_key_pressed) 
+	void Paddle::movePaddle(bool move_up_key_pressed, bool move_down_key_pressed, TimeService* time_service)
 	{
 		if (move_up_key_pressed && paddle_sprite.getPosition().y > topBoundary)
 		{
-			paddle_sprite.move(0, -paddleSpeed);
+			paddle_sprite.move(0, -paddleSpeed * time_service->getDeltaTime() * speedMultiplier);
 		}
 		if (move_down_key_pressed && paddle_sprite.getPosition().y + paddle_sprite.getSize().y < bottomBoundary)
 		{
-			paddle_sprite.move(0, paddleSpeed);
+			paddle_sprite.move(0, paddleSpeed * time_service->getDeltaTime() * speedMultiplier);
 		}
 	}
 
-	void Paddle::update(bool move_up_key_pressed, bool move_down_key_pressed) 
+	void Paddle::update(bool move_up_key_pressed, bool move_down_key_pressed, TimeService* time_service)
 	{
-		movePaddle(move_up_key_pressed, move_down_key_pressed);
+		movePaddle(move_up_key_pressed, move_down_key_pressed, time_service);
 	}
 
 	RectangleShape Paddle::getPaddleSprite() 
